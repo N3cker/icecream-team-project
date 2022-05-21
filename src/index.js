6d17aea5aedbf53b3de944268050f0bf9e7b0a7d
n3cker <piaszuu@gmail.com>
2022-05-21 19:26:28 +0200

    finish mobile slider

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import '@splidejs/splide/dist/css/splide.min.css'
 
 import Splide from '@splidejs/splide';
 
-new Splide( '.splide' ).mount();
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    
+    new Splide( '.splide', {
+        arrows: false,
+        // autoplay: true,
+        // type: "loop"
+    } ).mount();
+    
+  } );
